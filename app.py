e2f666836d47ca5c017b226419f9fa8e600916d2
meisyaamanda <91866651+meisyaamanda@users.noreply.github.com>
2023-08-06 22:16:32 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
+
 from flask import Flask, request, jsonify
 import pickle
-import  numpy as np
+import numpy as np
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Mengizinkan permintaan dari domain yang berbeda (untuk komunikasi dengan Flutter)
+cors = CORS(app, resources={r"/predict": {"origins": "*"}})
 
-@app.route('/', methods=['POST'])
-def predict_janin():
-    with open('rf_pickle.pkl', 'rb') as r:
-        model = pickle.load(r)
+# Load model saat aplikasi dimulai
+with open('rf_pickle.pkl', 'rb') as r:
+    model = pickle.load(r, encoding='latin1')
 
-    data = request.get_json()
-    accelerations = float(data['accelerations'])
-    movement = float(data['movement'])
-    uterine = float(data['uterine'])
-    light = float(data['light'])
-    severe = float(data['severe'])
-    prolongued = float(data['prolongued'])
-    abnormal = float(data['abnormal'])
-    percentage = float(data['percentage'])
 
-    datas = np.array((accelerations, movement, uterine, light, severe, prolongued, abnormal, percentage))
-    datas = np.reshape(datas, (1, -1))
+@app.route('/predict', methods=['POST'])
+# @cross_origin()
+def predict():
+    if request.method == 'POST':
+        try:
+            data = request.json
 
-    isJanin = model.predict(datas)
+            accelerations = float(data['accelerations'])
+            movement = float(data['movement'])
+            uterine = float(data['uterine'])
+            light = float(data['light'])
+            severe = float(data['severe'])
+            prolongued = float(data['prolongued'])
+            abnormal = float(data['abnormal'])
+            percentage = float(data['percentage'])
 
-    # Mengembalikan hasil prediksi dalam bentuk JSON
-    return jsonify({'isJanin': int(isJanin[0])})
+            input_data = np.array(
+                (accelerations, movement, uterine, light, severe, prolongued, abnormal, percentage))
+            input_data = np.reshape(input_data, (1, -1))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+            isJanin = model.predict(input_data)
+
+            # Konversi hasil prediksi ke integer
+            result = {'result': int(isJanin[0])}
+
+            return jsonify(result), 200
+
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run()
